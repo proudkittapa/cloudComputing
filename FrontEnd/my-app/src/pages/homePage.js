a93320a85621a92f9366b266847bf77df8b2996a
Pink <62011095@kmitl.ac.th>
2021-11-08 17:06:47 +0700

    -

--- FrontEnd/my-app/src/pages/homePage.js
+++ FrontEnd/my-app/src/pages/homePage.js
@@ -27,63 +27,63 @@ function HomePage(){
     }
     return(
         <body>        
-            <nav class="navbar">
-                <div class="container-fluid">
-                <a class="navbar-brand flex-fill">Bababook</a>
-                <div class="d-none d-lg-block flex-fill">
-                    <form class="d-flex ">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                        <button class="btn btn-secondary" type="submit"><i class="fas fa-search"></i> Search</button>
+            <nav className="navbar">
+                <div className="container-fluid">
+                <a className="navbar-brand flex-fill">Bababook</a>
+                <div className="d-none d-lg-block flex-fill">
+                    <form className="d-flex ">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                        <button className="btn btn-secondary" type="submit"><i className="fas fa-search"></i> Search</button>
                     </form>
                 </div>
-                <div class="dropdown flex-fill">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div className="dropdown flex-fill">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <form class="d-flex justify-content-center d-lg-none">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                            <button class="btn btn-secondary" type="submit"><i class="fas fa-search"></i></button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <form className="d-flex justify-content-center d-lg-none">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                            <button className="btn btn-secondary" type="submit"><i className="fas fa-search"></i></button>
                         </form>
-                        <a class="dropdown-item" href="#"><i class="fas fa-user"></i> Profile</a>
-                        <a class="dropdown-item" href="#"><i class="fas fa-book"></i> Library</a>
-                        <a class="dropdown-item" href="#"><i class="fas fa-cog"></i> Settings</a>
-                        <hr class="dropdown-divider"></hr>
-                        <a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"></i> Log Out</a>
+                        <a className="dropdown-item" href="#"><i className="fas fa-user"></i> Profile</a>
+                        <a className="dropdown-item" href="#"><i className="fas fa-book"></i> Library</a>
+                        <a className="dropdown-item" href="#"><i className="fas fa-cog"></i> Settings</a>
+                        <hr className="dropdown-divider"></hr>
+                        <a className="dropdown-item" href="#"><i className="fas fa-sign-out-alt"></i> Log Out</a>
 
                     </div>
                 </div>
                 </div>
             </nav>
-            <div class="container">
-                <div class="row row-book-container row-book-sm row-book-lg">
-                    <div class="col-auto">
-                        <img class="book-cover" src="https://i.imgur.com/Io2Jx0c.png"></img>
+            <div className="container">
+                <div className="row row-book-container row-book-sm row-book-lg">
+                    <div className="col-auto">
+                        <img className="book-cover" src="https://i.imgur.com/Io2Jx0c.png"></img>
                     </div>
-                    <div class="col-lg">
-                        <div class="d-flex align-items-end">
+                    <div className="col-lg">
+                        <div className="d-flex align-items-end">
                             <h1>{bookItem.book.name}</h1>
-                            <h2 class="separator"> • </h2>
+                            <h2 className="separator"> • </h2>
                             <h2>Firstname Lastname</h2>
                         </div>
-                        <div class ="hr">
-                            <h3>Rating: <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></h3>
+                        <div className ="hr">
+                            <h3>Rating: <i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="far fa-star"></i></h3>
                             {/* {bookItem.book.rating} use this variable*/}
                             <br>
                             </br>
-                            <div class="pricing-add d-flex align-items-end">
-                                <div class="d-flex">
+                            <div className="pricing-add d-flex align-items-end">
+                                <div className="d-flex">
                                     <h3>Price: </h3>
                                     <h3>฿{bookItem.book.price}</h3>
                                 </div>
-                                <button class="btn btn-success" type="submit"><i class="fas fa-plus"></i> Add Book</button>
+                                <button className="btn btn-success" type="submit"><i className="fas fa-plus"></i> Add Book</button>
                             </div>
 
                         </div>
                     </div>
                 </div>
                 <h2>Description</h2>
-                <div class="hr"></div>
-                <p class="book-desc"> {bookItem.book.description}</p>
+                <div className="hr"></div>
+                <p className="book-desc"> {bookItem.book.description}</p>
             </div>
         </body>
         
