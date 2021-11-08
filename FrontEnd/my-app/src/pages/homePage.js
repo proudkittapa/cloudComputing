03fdb5677dbe10c6ccebadec86d7aa7e4049c506
proudkittapa <proudy2013@gmail.com>
2021-11-08 18:28:23 +0700

    fix front end get book by id

--- FrontEnd/my-app/src/pages/homePage.js
+++ FrontEnd/my-app/src/pages/homePage.js
@@ -9,16 +9,25 @@ function HomePage(){
     }, [])
     // let {id} = useParams("id")
     // console.log("id:", id)
-    let {id} = "1"
+    let id = "1"
     console.log("id:", id)
     const getBook = async (e) =>{
         try{
             const response = await getBookById(id)
             // console.log(response.data.data)
-            if (response.status == 200){
-                setBookItems(response.data.data || [])
-                console.log(bookItem)
-                console.log(bookItem.book.book_id)
+            if (response.status === 200){
+                // console.log(response)
+                // console.log(response.data)
+                // console.log(response.data.data.book)
+                const res = response.data.data;
+                // const temp = {book_id: res.book_id}
+                // console.log("temp", temp)
+                // let temp2 = [res]
+                // setMenu((oldValue) => ({ ...oldValue, [name]: value }))
+                setBookItems(() => (res))
+                // await setBookItems.changeStateFunction();
+                console.log("bookItem",bookItem.book)
+                // console.log(bookItem.book.book_id)
             }
         }
         catch(error){
@@ -27,63 +36,63 @@ function HomePage(){
     }
     return(
         <body>        
-            <nav className="navbar">
-                <div className="container-fluid">
-                <a className="navbar-brand flex-fill">Bababook</a>
-                <div className="d-none d-lg-block flex-fill">
-                    <form className="d-flex ">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                        <button className="btn btn-secondary" type="submit"><i className="fas fa-search"></i> Search</button>
+            <nav class="navbar">
+                <div class="container-fluid">
+                <a class="navbar-brand flex-fill">Bababook</a>
+                <div class="d-none d-lg-block flex-fill">
+                    <form class="d-flex ">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                        <button class="btn btn-secondary" type="submit"><i class="fas fa-search"></i> Search</button>
                     </form>
                 </div>
-                <div className="dropdown flex-fill">
-                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div class="dropdown flex-fill">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     </button>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <form className="d-flex justify-content-center d-lg-none">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                            <button className="btn btn-secondary" type="submit"><i className="fas fa-search"></i></button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <form class="d-flex justify-content-center d-lg-none">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                            <button class="btn btn-secondary" type="submit"><i class="fas fa-search"></i></button>
                         </form>
-                        <a className="dropdown-item" href="#"><i className="fas fa-user"></i> Profile</a>
-                        <a className="dropdown-item" href="#"><i className="fas fa-book"></i> Library</a>
-                        <a className="dropdown-item" href="#"><i className="fas fa-cog"></i> Settings</a>
-                        <hr className="dropdown-divider"></hr>
-                        <a className="dropdown-item" href="#"><i className="fas fa-sign-out-alt"></i> Log Out</a>
+                        <a class="dropdown-item" href="#"><i class="fas fa-user"></i> Profile</a>
+                        <a class="dropdown-item" href="#"><i class="fas fa-book"></i> Library</a>
+                        <a class="dropdown-item" href="#"><i class="fas fa-cog"></i> Settings</a>
+                        <hr class="dropdown-divider"></hr>
+                        <a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"></i> Log Out</a>
 
                     </div>
                 </div>
                 </div>
             </nav>
-            <div className="container">
-                <div className="row row-book-container row-book-sm row-book-lg">
-                    <div className="col-auto">
-                        <img className="book-cover" src="https://i.imgur.com/Io2Jx0c.png"></img>
+            <div class="container">
+                <div class="row row-book-container row-book-sm row-book-lg">
+                    <div class="col-auto">
+                        <img class="book-cover" src="https://i.imgur.com/Io2Jx0c.png"></img>
                     </div>
-                    <div className="col-lg">
-                        <div className="d-flex align-items-end">
+                    <div class="col-lg">
+                        <div class="d-flex align-items-end">
                             <h1>{bookItem.book.name}</h1>
-                            <h2 className="separator"> • </h2>
+                            <h2 class="separator"> • </h2>
                             <h2>Firstname Lastname</h2>
                         </div>
-                        <div className ="hr">
-                            <h3>Rating: <i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="far fa-star"></i></h3>
-                            {/* {bookItem.book.rating} use this variable*/}
+                        <div class ="hr">
+                            <h3>Rating: <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></h3>
+                            {/* {bookItem.book.rating} */}
                             <br>
                             </br>
-                            <div className="pricing-add d-flex align-items-end">
-                                <div className="d-flex">
+                            <div class="pricing-add d-flex align-items-end">
+                                <div class="d-flex">
                                     <h3>Price: </h3>
                                     <h3>฿{bookItem.book.price}</h3>
                                 </div>
-                                <button className="btn btn-success" type="submit"><i className="fas fa-plus"></i> Add Book</button>
+                                <button class="btn btn-success" type="submit"><i class="fas fa-plus"></i> Add Book</button>
                             </div>
 
                         </div>
                     </div>
                 </div>
                 <h2>Description</h2>
-                <div className="hr"></div>
-                <p className="book-desc"> {bookItem.book.description}</p>
+                <div class="hr"></div>
+                <p class="book-desc"> {bookItem.book.description}</p>
             </div>
         </body>
         
