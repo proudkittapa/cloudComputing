75f48ade3ea2c1780efd4839f31c21c39ba7fa35
Fluke Korndanai <flukekd@gmail.com>
2021-11-08 16:31:29 +0700

    updated book functions
    create book
    update book
    delete book

--- BackEnd/repository/book.go
+++ BackEnd/repository/book.go
@@ -56,7 +56,7 @@ func (repo *BookRepository) GetAll(c context.Context) ([]entity.Book, error) {
 }
 
 func (repo *BookRepository) GetById(c context.Context, id string) (entity.Book, error) {
-	id = "1"
+
 	result, err := repo.db.GetItem(&dynamodb.GetItemInput{
 		TableName: aws.String("book"),
 		Key: map[string]*dynamodb.AttributeValue{
@@ -96,21 +96,103 @@ func (repo *BookRepository) GetById(c context.Context, id string) (entity.Book, 
 func (repo *BookRepository) CreateBook(c context.Context, book entity.Book) error {
 	book.Id = GenerateUUID()
 	//insert to table book
+
+	item := entity.Book{
+		Id:          book.Id,
+		Name:        "Book name",
+		UserId:      "userId",
+		Price:       90,
+		Rating:      5.0,
+		Description: "No Description",
+	}
+
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		log.Fatalf("Got error marshalling new movie item: %s", err)
+	}
+
+	tableName := "book"
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = repo.db.PutItem(input)
+	if err != nil {
+		log.Fatalf("Got error calling PutItem: %s", err)
+	}
+
+	fmt.Println("Successfully added '" + item.Name + "' (" + item.Id + ") to table " + tableName)
 	return nil
 }
 
 func (repo *BookRepository) UpdateBook(c context.Context, book entity.Book) error {
+	// input: id string
 	//update
+	tableName := "book"
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":name": {
+				S: aws.String(book.Name),
+			},
+			":user_id": {
+				S: aws.String(book.UserId),
+			},
+			":price": {
+				N: aws.String(fmt.Sprintf("%f", book.Price)),
+			},
+			":rating": {
+				N: aws.String(fmt.Sprintf("%f", book.Rating)),
+			},
+			":description": {
+				S: aws.String(book.Description),
+			},
+		},
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"book_id": {
+				S: aws.String(book.Id),
+			},
+		},
+		ReturnValues: aws.String("UPDATED_NEW"),
+		// UpdateExpression: aws.String("set Rating = :r"),
+	}
+
+	_, err := repo.db.UpdateItem(input)
+	if err != nil {
+		log.Fatalf("Got error calling UpdateItem: %s", err)
+	}
+
+	fmt.Println("Successfully updated '" + book.Name + "' (" + book.Id)
 	return nil
 }
 
 func (repo *BookRepository) DeleteBook(c context.Context, id string) error {
 	//delete book with the given id
+	tableName := "book"
+
+	input := &dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"book_id": {
+				S: aws.String(id),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+
+	_, err := repo.db.DeleteItem(input)
+	if err != nil {
+		log.Fatalf("Got error calling DeleteItem: %s", err)
+	}
+
+	fmt.Println("Deleted Book Id'" + id + ") from table " + tableName)
 	return nil
 }
 
 func (repo *BookRepository) AddBook(c context.Context, userId string, bookId string) error {
 	//this is when user want to add book
 	//click add book
+
 	return nil
 }
