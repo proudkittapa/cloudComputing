c5d0e7a0cfabdb815985170ba8579ee7dccf7316
paminicha <pamnichapatr@gmail.com>
2021-11-27 12:37:59 +0700

    Update createprofile.js

--- FrontEnd/my-app/src/pages/createprofile.js
+++ FrontEnd/my-app/src/pages/createprofile.js
@@ -23,28 +23,12 @@ function CreateProfile(){
                     <div className="d-flex flex-fill align-items-center">
 
                         <a href="/" className="px-3"></a>
-
-                        <a href="/">
-                            <svg  version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-                            viewBox="0 0 64 64" style={{"enable-background":"new 0 0 64 64", width:"32px", height:"32px", xmlSpace:"preserve"}}>
-                                <polyline className="st0" points="31.63,44.51 54.52,61.28 54.5,61 54.5,2.5 9.5,2.5 9.5,61 9.48,61.28 32.23,44.61 "/>
-                                <polygon className="st1" points="32,11.55 35.89,19.42 44.58,20.69 38.29,26.82 39.77,35.47 32,31.39 24.23,35.47 25.71,26.82 
-                                    19.42,20.69 28.11,19.42 "/>
-                            </svg>
-                        </a>
                         
                         <div className="flex-fill">
                             <div className="nav-item dropdown">
                                 <button className="btn btn-navbar rounded-circle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <i className="fas fa-user"></i>
                                 </button>
-                                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <Link to={{pathname:`/user/${userId}`}}><a className="dropdown-item" href="#"><i className="fas fa-user"></i> Profile</a></Link>
-                                    <a className="dropdown-item" href="#"><i className="fas fa-book"></i> Account</a>
-                                    <a className="dropdown-item" href="#"><i className="fas fa-cog"></i> Settings</a>
-                                    <hr className="dropdown-divider"></hr>
-                                    <a className="dropdown-item" href="#"><i className="fas fa-sign-out-alt"></i> Log Out</a>
-                                </div>
                             </div>
                         </div>
                     </div>
