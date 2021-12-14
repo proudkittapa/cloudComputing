991526c4d07c90a99fdd267f49ec5caedfe675de
proudkittapa <proudy2013@gmail.com>
2021-12-15 00:09:22 +0700

    Update main.go

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -30,7 +30,8 @@ func main() {
 	//fmt.Println("svc", svc)
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"http://18.138.251.129:3000/", "http://18.138.251.129:3000", "18.138.251.129", "http://18.138.251.129", "http://ec2-18-138-251-129.ap-southeast-1.compute.amazonaws.com:3000/", "http://ec2-18-138-251-129.ap-southeast-1.compute.amazonaws.com:3000"},
+		//AllowOrigins:     []string{"http://18.138.251.129:3000/", "http://18.138.251.129:3000", "18.138.251.129", "http://18.138.251.129", "http://ec2-18-138-251-129.ap-southeast-1.compute.amazonaws.com:3000/", "http://ec2-18-138-251-129.ap-southeast-1.compute.amazonaws.com:3000"},
+		AllowOrigins: []string{"*"},
 		AllowCredentials: true,
 		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, http.MethodOptions},
 		//AllowHeaders: []string{"*"},
