9a7d240b2dc589f6a29d1c611e59ebee68979e37
proudkittapa <proudy2013@gmail.com>
2021-12-15 00:08:11 +0700

    Update main.go

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -32,7 +32,7 @@ func main() {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins:     []string{"http://18.138.251.129:3000/", "http://18.138.251.129:3000", "18.138.251.129", "http://18.138.251.129", "http://ec2-18-138-251-129.ap-southeast-1.compute.amazonaws.com:3000/", "http://ec2-18-138-251-129.ap-southeast-1.compute.amazonaws.com:3000"},
 		AllowCredentials: true,
-		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, http.MethodOptions},
 		//AllowHeaders: []string{"*"},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAllow, echo.HeaderContentLength, echo.HeaderContentEncoding, echo.HeaderAccessControlAllowOrigin, echo.HeaderAccessControlAllowMethods, echo.HeaderAccessControlRequestHeaders, "User-Agent", "Sec-Fetch-Mode", "Referer"},
 		ExposeHeaders: []string{"*"},
@@ -55,5 +55,5 @@ func main() {
 	handler.NewUserHandler(handlerGroup, userUseCase)
 	handler.NewUserTransactionHandler(handlerGroup, userTransactionUseCase)
 	// handler.NewAuthHandler(authGroup)
-	e.Logger.Fatal(e.StartAutoTLS(":8080"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
