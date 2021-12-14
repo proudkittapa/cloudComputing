3e905c1750ccd43e00dff0bd5c9db7c5477f39a9
proudkittapa <proudy2013@gmail.com>
2021-12-14 15:18:55 +0700

    change cors

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -33,7 +33,7 @@ func main() {
 	//fmt.Println("svc", svc)
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8080"},
+		AllowOrigins:     []string{"http://52.221.200.38:3000", "http://52.221.200.38:8080"},
 		AllowCredentials: true,
 		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
