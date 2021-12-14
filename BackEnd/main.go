57f21f12681e0abc3b395b88e729e224f9f184de
proudkittapa <proudy2013@gmail.com>
2021-12-14 21:15:57 +0700

    Update main.go

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -6,11 +6,13 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/proudkittapa/cloudComputing/config"
 	"github.com/proudkittapa/cloudComputing/handler"
 	repo "github.com/proudkittapa/cloudComputing/repository"
 	"github.com/proudkittapa/cloudComputing/usecase"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -27,12 +29,12 @@ func main() {
 	svc := dynamodb.New(sess)
 	//fmt.Println("svc", svc)
 	e := echo.New()
-	//e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	//	AllowOrigins:     []string{"http://52.221.200.38:3000", "http://52.221.200.38:8080", "http://172.38.1.98:3000", "http://172.38.1.98:8080", "http://ec2-52-221-200-38.ap-southeast-1.compute.amazonaws.com:3000/"},
-	//	AllowCredentials: true,
-	//	AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
-	//	AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAllow, echo.HeaderContentLength, echo.HeaderContentEncoding, echo.HeaderAccessControlAllowOrigin, echo.HeaderAccessControlAllowMethods, echo.HeaderAccessControlRequestHeaders, "User-Agent", "Sec-Fetch-Mode", "Referer"},
-	//}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://52.221.200.38:3000"},
+		AllowCredentials: true,
+		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAllow, echo.HeaderContentLength, echo.HeaderContentEncoding, echo.HeaderAccessControlAllowOrigin, echo.HeaderAccessControlAllowMethods, echo.HeaderAccessControlRequestHeaders, "User-Agent", "Sec-Fetch-Mode", "Referer"},
+	}))
 	handlerGroup := e.Group("/bababook")
 	// authGroup := e.Group("/oauth")
 
