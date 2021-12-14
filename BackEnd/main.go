de7575854ca7f47ef44e1b01bca2cb2f2f384a3f
proudkittapa <proudy2013@gmail.com>
2021-12-14 20:37:21 +0700

    Update main.go

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -34,7 +34,7 @@ func main() {
 		AllowOrigins:     []string{"http://52.221.200.38:3000", "http://52.221.200.38:8080", "http://172.38.1.98:3000", "http://172.38.1.98:8080", "http://ec2-52-221-200-38.ap-southeast-1.compute.amazonaws.com:3000/"},
 		AllowCredentials: true,
 		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
-		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAllow, echo.HeaderContentLength, echo.HeaderContentEncoding, echo.HeaderAccessControlAllowOrigin, echo.HeaderAccessControlAllowMethods, echo.HeaderAccessControlRequestHeaders, "User-Agent", "Sec-Fetch-Mode"},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAllow, echo.HeaderContentLength, echo.HeaderContentEncoding, echo.HeaderAccessControlAllowOrigin, echo.HeaderAccessControlAllowMethods, echo.HeaderAccessControlRequestHeaders, "User-Agent", "Sec-Fetch-Mode", "Referer"},
 	}))
 	handlerGroup := e.Group("/bababook")
 	// authGroup := e.Group("/oauth")
