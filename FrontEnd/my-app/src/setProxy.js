b320b8bc2f74a53ddcccef7fb215ffffa0c4614e
proudkittapa <proudy2013@gmail.com>
2021-12-14 23:08:02 +0700

    Update setProxy.js

--- FrontEnd/my-app/src/setProxy.js
+++ FrontEnd/my-app/src/setProxy.js
@@ -1,5 +1,17 @@
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function (app) {
+//   app.use(createProxyMiddleware(['/bababook'], { target: 'http://172.38.20.142:8080' }));
+// };
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  app.use(createProxyMiddleware(['/bababook'], { target: 'http://172.38.20.142:8080' }));
+module.exports = function(app) {
+  app.use(
+    // '/AdoptMe',
+    createProxyMiddleware({
+      target: 'http://0.0.0.0:3000/',
+      changeOrigin: true,
+    })
+  );
 };
