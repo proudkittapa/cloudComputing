b0e6ec42f57168a74557127c9c41c6dd7a0f8bf0
proudkittapa <proudy2013@gmail.com>
2021-12-14 19:58:00 +0700

    add cors

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/labstack/echo/middleware"
 	"github.com/proudkittapa/cloudComputing/config"
 	"log"
+	"net/http"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -28,12 +30,12 @@ func main() {
 	svc := dynamodb.New(sess)
 	//fmt.Println("svc", svc)
 	e := echo.New()
-	//e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	//	AllowOrigins:     []string{"http://52.221.200.38:3000", "http://52.221.200.38:8080"},
-	//	AllowCredentials: true,
-	//	AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
-	//	AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-	//}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://52.221.200.38:3000", "http://52.221.200.38:8080", "http://172.38.1.98:3000"},
+		AllowCredentials: true,
+		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 	handlerGroup := e.Group("/bababook")
 	// authGroup := e.Group("/oauth")
 
