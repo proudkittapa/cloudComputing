b5ae10736098b045913452cf0401ce59dcf7579b
proudkittapa <proudy2013@gmail.com>
2021-12-15 00:31:32 +0700

    change proxy

--- FrontEnd/my-app/src/setProxy.js
+++ FrontEnd/my-app/src/setProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(createProxyMiddleware(['/bababook'], { target: 'http://172.38.20.142:8080' }));
+  app.use(createProxyMiddleware({ target: 'http://172.38.20.142:8080', changeOrigin: true, }));
 };
 
 // const { createProxyMiddleware } = require('http-proxy-middleware');
