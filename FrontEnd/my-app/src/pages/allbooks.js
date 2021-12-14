89af24fad5bcab510b8a33826a9a1c8ffccb81da
proudkittapa <proudy2013@gmail.com>
2021-12-15 00:38:14 +0700

    Update allbooks.js

--- FrontEnd/my-app/src/pages/allbooks.js
+++ FrontEnd/my-app/src/pages/allbooks.js
@@ -27,15 +27,43 @@ function AllBook(){
         })
     }
 
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
     const getAllBooks = () => {
         console.log("getAllBooks")
-        axios.get(`http://172.38.20.142:8080/bababook/book`)
-        .then((response) =>{
-            setBooks(response.data.data.books)
-            console.log("books", books)
-        }).catch(function (error) {
-            console.log("error", error)
-        })
+        let url = "http://172.38.20.142:8080/bababook/book"
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+              'Content-Type': 'application/json'
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+          });
+        setBooks(response.data.data.books)
+        // axios.get(`http://172.38.20.142:8080/bababook/book`)
+        // .then((response) =>{
+        //     setBooks(response.data.data.books)
+        //     console.log("books", books)
+        // }).catch(function (error) {
+        //     console.log("error", error)
+        // })
     }
 
     const getAuthors = () =>{
