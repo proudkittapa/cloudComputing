3ba9190a165bc1b4ae787cf27e2fbaf4987641b9
proudkittapa <proudy2013@gmail.com>
2021-12-15 00:41:16 +0700

    Update allbooks.js

--- FrontEnd/my-app/src/pages/allbooks.js
+++ FrontEnd/my-app/src/pages/allbooks.js
@@ -27,19 +27,6 @@ function AllBook(){
         })
     }
 
-    const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-      });
     const getAllBooks = () => {
         console.log("getAllBooks")
         let url = "http://172.38.20.142:8080/bababook/book"
