5e4c6f8ce744e67f30f5de19e753c81103b9452d
proudkittapa <proudy2013@gmail.com>
2021-12-15 00:42:16 +0700

    Update allbooks.js

--- FrontEnd/my-app/src/pages/allbooks.js
+++ FrontEnd/my-app/src/pages/allbooks.js
@@ -31,7 +31,7 @@ function AllBook(){
         console.log("getAllBooks")
         let url = "http://172.38.20.142:8080/bababook/book"
         const response =  fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, *cors, same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             credentials: 'same-origin', // include, *same-origin, omit
@@ -41,7 +41,7 @@ function AllBook(){
             },
             redirect: 'follow', // manual, *follow, error
             referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify(data) // body data type must match "Content-Type" header
+            // body: JSON.stringify(data) // body data type must match "Content-Type" header
           });
         setBooks(response.data.data.books)
         // axios.get(`http://172.38.20.142:8080/bababook/book`)
