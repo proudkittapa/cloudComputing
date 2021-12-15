a4ded692c2b843c79e953d4d6335ba93e5795f4a
proudkittapa <proudy2013@gmail.com>
2021-12-15 11:10:56 +0700

    Update setProxy.js

--- FrontEnd/my-app/src/setProxy.js
+++ FrontEnd/my-app/src/setProxy.js
@@ -1,8 +1,8 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  app.use(createProxyMiddleware({ target: 'http://localhost:8080', changeOrigin: true, }));
-};
+// module.exports = function (app) {
+//   app.use(createProxyMiddleware({ target: 'http://localhost:8080', changeOrigin: true, }));
+// };
 
 // const { createProxyMiddleware } = require('http-proxy-middleware');
 
